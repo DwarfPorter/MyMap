822acfc6f0ef7ff89a580b9fac0e1c14b7acc17c
DwarfPorter <mrvlmor@gmail.com>
2020-06-05 21:38:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/mymap/MapsActivity.java
@@ -0,0 +1,270 @@
+package ru.geekbrains.mymap;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final int PERMISSION_REQUEST_CODE = 10;
+
+    private EditText textLatitude;
+    private EditText textLongitude;
+    private TextView textAddress;
+
+    private GoogleMap mMap;
+    private Marker currentMarker;
+    private List<Marker> markers = new ArrayList<Marker>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        initViews();
+        requestPemissions();
+    }
+
+    // Инициализация Views
+    private void initViews() {
+        textLatitude = findViewById(R.id.editLat);
+        textLongitude = findViewById(R.id.editLng);
+        textAddress = findViewById(R.id.textAddress);
+        initSearchByAddress();
+    }
+
+    private void initSearchByAddress() {
+        final EditText textSearch = findViewById(R.id.searchAddress);
+        findViewById(R.id.buttonSearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Geocoder geocoder = new Geocoder(MapsActivity.this);
+                final String searchText = textSearch.getText().toString();
+                // Операция получения занимает некоторое время и работает по
+                // интернету. Поэтому её необходимо запускать в отдельном потоке
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // Получаем координаты по адресу
+                            List<Address> addresses = geocoder.getFromLocationName(searchText, 1);
+                            if (addresses.size() > 0){
+                                final LatLng location = new LatLng(addresses.get(0).getLatitude(),
+                                        addresses.get(0).getLongitude());
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mMap.addMarker(new MarkerOptions()
+                                                .position(location)
+                                                .title(searchText)
+                                                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_search_marker)));
+                                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, (float)15));
+                                    }
+                                });
+                            }
+                            else{
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "Не нашли", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        currentMarker = mMap.addMarker(new MarkerOptions().position(sydney).title("Текущая позиция")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_round)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                addMarker(latLng);
+                getAddress(latLng);
+                drawLine();
+            }
+        });
+
+    }
+
+    private void drawLine(){
+        int last = markers.size() - 1;
+        if (last >= 1){
+            LatLng previous = markers.get(last-1).getPosition();
+            LatLng current = markers.get(last).getPosition();
+            mMap.addPolyline(new PolylineOptions()
+            .add(previous, current)
+            .color(Color.RED)
+            .width(5));
+        }
+    }
+
+    // Добавляем метки на карту
+    private void addMarker(LatLng location){
+        String title = Integer.toString(markers.size());
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(location)
+                .title(title)
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker)));
+        markers.add(marker);
+    }
+
+    // Получаем адрес по координатам
+    private void getAddress(final LatLng location){
+        final Geocoder geocoder = new Geocoder(this);
+        // Поскольку Geocoder работает по интернету, создаём отдельный поток
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final List<Address> addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1);
+                    textAddress.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textAddress.setText(addresses.get(0).getAddressLine(0));
+                        }
+                    });
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void requestPemissions() {
+        // Проверим, есть ли Permission’ы, и если их нет, запрашиваем их у
+        // пользователя
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Запрашиваем координаты
+            requestLocation();
+        } else {
+            // Permission’ов нет, запрашиваем их у пользователя
+            requestLocationPermissions();
+        }
+    }
+
+    // Запрашиваем Permission’ы для геолокации
+    private void requestLocationPermissions() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+            // Запрашиваем эти два Permission’а у пользователя
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    // Запрашиваем координаты
+    private void requestLocation() {
+        // Если Permission’а всё- таки нет, просто выходим: приложение не имеет
+        // смысла
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return;
+        // Получаем менеджер геолокаций
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        // Получаем наиболее подходящий провайдер геолокации по критериям.
+        // Но определить, какой провайдер использовать, можно и самостоятельно.
+        // В основном используются LocationManager.GPS_PROVIDER или
+        // LocationManager.NETWORK_PROVIDER, но можно использовать и
+        // LocationManager.PASSIVE_PROVIDER - для получения координат в
+        // пассивном режиме
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (provider != null) {
+            // Будем получать геоположение через каждые 10 секунд или каждые
+            // 10 метров
+            locationManager.requestLocationUpdates(provider, 20, 1, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double lat = location.getLatitude(); // Широта
+                    String latitude = Double.toString(lat);
+                    textLatitude.setText(latitude);
+
+                    double lng = location.getLongitude(); // Долгота
+                    String longitude = Double.toString(lng);
+                    textLongitude.setText(longitude);
+
+                    String accuracy = Float.toString(location.getAccuracy());   // Точность
+
+                    LatLng currentPosition = new LatLng(lat, lng);
+                    currentMarker.setPosition(currentPosition);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, (float)15));
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                }
+            });
+        }
+    }
+
+}
